668ec2e2783f942eca47b23cf8f6e1b3367408d1
Kamil Abbas <kamilabbas929@gmail.com>
2026-02-19 21:36:01 +0500

    fix stripe connect

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,21 +3,18 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
 
-let cached = (global as any).mongoose as
-  | { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null }
-  | undefined;
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-if (!cached) cached = (global as any).mongoose = { conn: null, promise: null };
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
 
-export async function dbConnect() {
-  if (cached!.conn) return cached!.conn;
-
-  if (!cached!.promise) {
-    cached!.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
   }
 
-  cached!.conn = await cached!.promise;
-  return cached!.conn;
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
+
+export { dbConnect };
+export default dbConnect; // <- add this
