9b0b281fa65865c0626cdf484e17fc4988ee1423
kamil-Abbas12 <kamilabbas929@gmail.com>
2026-07-04 21:32:29 +0500

    Remove pricing URL from sitemap
    Removed the pricing URL from the sitemap.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -30,7 +30,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: baseUrl, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
     { url: `${baseUrl}/about`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
     { url: `${baseUrl}/contact`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
-    { url: `${baseUrl}/pricing`, lastModified: new Date(), changeFrequency: "weekly", priority: 0.9 },
     { url: `${baseUrl}/solution/pay-per-call`, lastModified: new Date(), changeFrequency: "weekly", priority: 0.9 },
     { url: `${baseUrl}/services`, lastModified: new Date(), changeFrequency: "weekly", priority: 0.9 },
     { url: `${baseUrl}/services/insurances-leads`, lastModified: new Date(), changeFrequency: "weekly", priority: 0.9 },
