b7cc39afc5c857ab89864ad6287114412360679a
Kamil Abbas <kamilabbas929@gmail.com>
2026-09-11 01:33:24 +0500

    Remove unused public files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ConsentBanner from "./components/ConsentBanner";
 import Script from "next/script";
+// @ts-expect-error Global CSS is handled by Next.js during bundling.
 import "./globals.css";
 import ChunkErrorHandler from "./chunk-error-handler";
 
